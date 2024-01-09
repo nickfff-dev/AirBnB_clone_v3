111f1661eb90bf222a05140504269f556aa9e7a4
nickfff-dev <hunchographics@gmail.com>
2024-01-10 00:15:49 +0300

    search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,8 @@ from flask import jsonify, abort, request, make_response
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 
@@ -80,3 +82,51 @@ def update_place(place_id):
             setattr(place, key, value)
     storage.save()
     return jsonify(place.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def places_search():
+    """Retrieves all Place objects
+    depending on the JSON in the body of the request."""
+    body = request.get_json()
+    if body is None:
+        abort(400, 'Not a JSON')
+
+    # If the JSON body is empty or each list of all keys are empty,
+    # retrieve all Place objects.
+    if not body or all(not body.get(key) for
+                       key in ['states', 'cities', 'amenities']):
+        places = storage.all(Place).values()
+    else:
+        places = []
+
+        # If states list is not empty,
+        # include all Place objects for each State id listed
+        if body.get('states'):
+            for state_id in body['states']:
+                state = storage.get(State, state_id)
+                if state:
+                    for city in state.cities:
+                        places.extend(city.places)
+
+        # If cities list is not empty,
+        # include all Place objects for each City id listed
+        if body.get('cities'):
+            for city_id in body['cities']:
+                city = storage.get(City, city_id)
+                if city and city not in [place.city for place in places]:
+                    places.extend(city.places)
+
+        # Remove duplicates
+        places = list(set(places))
+
+        # If amenities list is not empty, filter Place objects by amenities
+        if body.get('amenities'):
+            amenities_ids = body['amenities']
+            places = [place for place in places if
+                      all(amenity.id in amenities_ids
+                          for amenity in place.amenities)]
+
+    # Return the results
+    places_list = [place.to_dict() for place in places]
+    return jsonify(places_list)
